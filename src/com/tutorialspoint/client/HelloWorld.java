833a27e1f98a241bb896139a64d6b969f140c010
HugoGustavo <hugog.valin@gmail.com>
2018-09-06 15:49:49 -0300

    Hidden Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -6,53 +6,43 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FileUpload;
-import com.google.gwt.user.client.ui.FormPanel;
-import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Hidden;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		VerticalPanel panel = new VerticalPanel();
-		final FormPanel form = new FormPanel();
-		final FileUpload fileUpload = new FileUpload();
-		Label selectLabel = new Label("Select a file: ");
-		Button uploadButton = new Button("Upload File: ");
+		final TextBox textBox = new TextBox();
+		textBox.setWidth("275");
+		Button button1 = new Button("Set Value of Hidden Input");
+		Button button2 = new Button("Get Value of Hidden Input");
 		
-		form.setAction("http://www.tutorialspoint.com/gwt/myFormHandler");
-		form.setEncoding(FormPanel.ENCODING_MULTIPART);
-		form.setMethod(FormPanel.METHOD_POST);
+		final Hidden hidden = new Hidden();
 		
-		panel.add(selectLabel);
-		panel.add(fileUpload);
-		panel.add(uploadButton);
-		uploadButton.addClickHandler(new ClickHandler() {
+		button1.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				String filename = fileUpload.getFilename();
-				
-				if ( filename.length() == 0 ) {
-					Window.alert("No File Specified!");
-				} else {
-					form.submit();
-				}
+				hidden.setValue(textBox.getValue());
+				Window.alert("Value of Hidden Widget Updated!");
 			}
 		});
 		
-		form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
-			public void onSubmitComplete(SubmitCompleteEvent event) {
-				Window.alert(event.getResults());
+		button2.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				Window.alert("Value of Hidden Widget: " + hidden.getValue());
 			}
 		});
 		
+		VerticalPanel panel = new VerticalPanel();
 		panel.setSpacing(10);
-
-		form.add(panel);
+		panel.add(textBox);
+		panel.add(button1);
+		panel.add(hidden);
+		panel.add(button2);
 		
-		RootPanel.get("gwtContainer").add(form);
+		RootPanel.get("gwtContainer").add(panel);
 	}
 
 }
