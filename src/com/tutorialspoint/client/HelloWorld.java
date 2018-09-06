75d53a6ea8e934d045b6050d89c141a70e30c832
HugoGustavo <hugog.valin@gmail.com>
2018-09-06 15:41:47 -0300

    FileUpload Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,7 +2,14 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RichTextArea;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FileUpload;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -10,17 +17,42 @@ public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		RichTextArea richTextArea = new RichTextArea();
-		
-		richTextArea.setHeight("220");
-		richTextArea.setWidth("200");
-		
-		richTextArea.setHTML("<b>Hello World!<b><br/><br/>" +
-		"<i> Be Happy!</i><br/></br><u>Stay Cool!</u>");
-		
 		VerticalPanel panel = new VerticalPanel();
-		panel.add(richTextArea);
-		RootPanel.get("gwtContainer").add(panel);
+		final FormPanel form = new FormPanel();
+		final FileUpload fileUpload = new FileUpload();
+		Label selectLabel = new Label("Select a file: ");
+		Button uploadButton = new Button("Upload File: ");
+		
+		form.setAction("http://www.tutorialspoint.com/gwt/myFormHandler");
+		form.setEncoding(FormPanel.ENCODING_MULTIPART);
+		form.setMethod(FormPanel.METHOD_POST);
+		
+		panel.add(selectLabel);
+		panel.add(fileUpload);
+		panel.add(uploadButton);
+		uploadButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				String filename = fileUpload.getFilename();
+				
+				if ( filename.length() == 0 ) {
+					Window.alert("No File Specified!");
+				} else {
+					form.submit();
+				}
+			}
+		});
+		
+		form.addSubmitCompleteHandler(new FormPanel.SubmitCompleteHandler() {
+			public void onSubmitComplete(SubmitCompleteEvent event) {
+				Window.alert(event.getResults());
+			}
+		});
+		
+		panel.setSpacing(10);
+
+		form.add(panel);
+		
+		RootPanel.get("gwtContainer").add(form);
 	}
 
 }
