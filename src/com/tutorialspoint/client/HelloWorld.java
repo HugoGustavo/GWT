327adfc6a5e0387658f06161ae50dc27fd63efb4
HugoGustavo <hugog.valin@gmail.com>
2018-09-06 08:21:39 -0300

    Anchor Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,7 +2,7 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -10,13 +10,9 @@ public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Image image = new Image();
-		
-		image.setUrl("http://www.tutorialspoint.com/images/gwt-mini.png");
-		
+		Anchor anchor = new Anchor("TutorialsPoint", false, "http://www.tutorialspoint.com", "_blank");
 		VerticalPanel panel = new VerticalPanel();
-		panel.add(image);
-		
+		panel.add(anchor);
 		RootPanel.get("gwtContainer").add(panel);
 	}
 
