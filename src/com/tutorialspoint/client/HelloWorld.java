3ea5f07385ee05bc6dbf7e685ca7d962b8af7a4c
HugoGustavo <hugog.valin@gmail.com>
2018-09-06 15:09:33 -0300

    PasswordTextBox Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,44 +2,29 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		MultiWordSuggestOracle oracle = new MultiWordSuggestOracle();
-		oracle.add("A");
-		oracle.add("AB");
-		oracle.add("ABC");
-		oracle.add("ABCD");
-		oracle.add("B");
-		oracle.add("BC");
-		oracle.add("BCD");
-		oracle.add("BCDE");
-		oracle.add("C");
-		oracle.add("CD");
-		oracle.add("CDE");
-		oracle.add("CDEF");
-		oracle.add("D");
-		oracle.add("DE");
-		oracle.add("DEF");
-		oracle.add("DEFG");
+		PasswordTextBox passwordTextBox1 = new PasswordTextBox();
+		PasswordTextBox passwordTextBox2 = new PasswordTextBox();
 		
-		SuggestBox suggestionBox = new SuggestBox(oracle);
+		passwordTextBox2.setText("hello@W@rld");
 		
-		suggestionBox.setWidth("200");
+		passwordTextBox2.setReadOnly(true);
 		
-		VerticalPanel panel= new VerticalPanel();
+		VerticalPanel panel = new VerticalPanel();
 		
-		panel.add(suggestionBox);
+		panel.setSpacing(10);
+		panel.add(passwordTextBox1);
+		panel.add(passwordTextBox2);
+
 		
 		RootPanel.get("gwtContainer").add(panel);
-		
-		
 	}
 
 }
