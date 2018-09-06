518a2a79633ca8ebb7b8d63f363c647aedf147f1
HugoGustavo <hugog.valin@gmail.com>
2018-09-06 08:32:35 -0300

    Button Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,7 +2,10 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -10,9 +13,40 @@ public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		Anchor anchor = new Anchor("TutorialsPoint", false, "http://www.tutorialspoint.com", "_blank");
+		Button redButton = new Button("Red");
+		Button greenButton = new Button("Green");
+		Button blueButton = new Button("Blue");
+		
+		redButton.setWidth("100px");
+		greenButton.setWidth("100px");
+		blueButton.setWidth("100px");
+		
+		greenButton.addStyleName("gwt-Green-Button");
+		blueButton.addStyleName("gwt-Blue-Button");
+		
+		redButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				Window.alert("Red button clicked!");
+			}
+		});
+				
+		greenButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				Window.alert("Green Button clicked");
+			}
+		});
+		
+		blueButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				Window.alert("Blue Button clicked");
+			}
+		});
+		
 		VerticalPanel panel = new VerticalPanel();
-		panel.add(anchor);
+		panel.setSpacing(10);
+		panel.add(redButton);
+		panel.add(greenButton);
+		panel.add(blueButton);
 		RootPanel.get("gwtContainer").add(panel);
 	}
 
