15ef9cf3abc1851de0efb5a41066d5d4bc9c09e5
HugoGustavo <hugog.valin@gmail.com>
2018-09-10 08:04:24 -0300

    CellList Widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -1,54 +1,74 @@
 package com.tutorialspoint.client;
 
 
-import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
 
+import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.i18n.client.DateTimeFormat;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.cellview.client.CellList;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.datepicker.client.DateBox;
-import com.google.gwt.user.datepicker.client.DatePicker;
+import com.google.gwt.view.client.ProvidesKey;
+import com.google.gwt.view.client.SelectionModel;
+import com.google.gwt.view.client.SingleSelectionModel;
 
 
 public class HelloWorld implements EntryPoint {
 
+	private static class Contact {
+		private static int nextId = 0;
+		private final int id;
+		private String name;
+		
+		public Contact(String name) {
+			nextId++;
+			this.id = nextId;
+			this.name = name;
+		}
+	}
 	
+	private static class ContactCell extends AbstractCell<Contact>{
+		@Override
+		public void render(Context context, Contact value, SafeHtmlBuilder sb) {
+			if ( value != null )
+				sb.appendEscaped(value.name);	
+		}
+	}
+	
+	private static final List<Contact> CONTACTS = Arrays.asList(new Contact("John"), new Contact("Joe"), new Contact("Michael"), new Contact("Sarah"), new Contact("George"));
 	@Override
 	public void onModuleLoad() {
-		DatePicker datePicker = new DatePicker();
-		final Label text = new Label();
-		
-		datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<Date> event) {
-				Date date = event.getValue();
-				String dateString = DateTimeFormat.getFormat("MM/dd/yyyy").format(date);
-				text.setText(dateString);
+		ProvidesKey<Contact> keyProvider = new ProvidesKey<Contact>() {
+			public Object getKey(Contact item) {
+				return (item == null) ? null : item.id;
 			}
-		});
+		};
 		
-		datePicker.setValue(new Date(), true);
+		CellList<Contact> cellList = new CellList<Contact>(new ContactCell(),keyProvider);
 		
-		DateTimeFormat dateFormat = DateTimeFormat.getFormat("MM/dd/yyyy");
-		DateBox dateBox = new DateBox();
-		dateBox.setFormat(new DateBox.DefaultFormat(dateFormat));
+		cellList.setRowCount(CONTACTS.size(), true);
+		cellList.setRowData(0, CONTACTS);
 		
-		Label selectLabel = new Label("Permanent DatePicker: ");
-		Label selectLabel2 = new Label("DateBox with popup DatePicker: ");
+		SelectionModel<Contact> selectionModel = new SingleSelectionModel<Contact>(keyProvider);
+		cellList.setSelectionModel(selectionModel);
 		
-		VerticalPanel vPanel = new VerticalPanel();
-		vPanel.setSpacing(10);
-		vPanel.add(selectLabel);
-		vPanel.add(text);
-		vPanel.add(datePicker);
-		vPanel.add(selectLabel2);
-		vPanel.add(dateBox);
+		Contact sarah = CONTACTS.get(3);
+		selectionModel.setSelected(sarah, true);
 		
-		RootPanel.get("gwtContainer").add(vPanel);
+		sarah.name = "Sara";
+		
+		cellList.redraw();
+		
+		VerticalPanel panel = new VerticalPanel();
+		panel.setBorderWidth(1);
+		panel.setWidth("200");
+		panel.add(cellList);
+		
+		RootPanel.get().add(panel);
+		
+		
 	}
 
 }
