c85a8fe3b084e90d2880a6031f2a0ffbecf660dc
HugoGustavo <hugog.valin@gmail.com>
2018-09-10 09:30:52 -0300

    HorizontalPanel

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -4,7 +4,7 @@ package com.tutorialspoint.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DecoratorPanel;
-import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 
@@ -12,17 +12,17 @@ public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		FlowPanel flowPanel = new FlowPanel();
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
 		
-		for ( int i = 1; i <=10; i++) {
-			CheckBox checkBox = new CheckBox("Item" + i);
-			flowPanel.add(checkBox);
+		for (int i = 1;  i<= 10; i++) {
+			CheckBox checkBox = new CheckBox("Item " + i);
+			horizontalPanel.add(checkBox);
 		}
 		
 		DecoratorPanel decoratorPanel = new DecoratorPanel();
 		decoratorPanel.setWidth("500");
-		decoratorPanel.add(flowPanel);
-
+		decoratorPanel.add(horizontalPanel);
+		
 		RootPanel.get("gwtContainer").add(decoratorPanel);
 	}
 
