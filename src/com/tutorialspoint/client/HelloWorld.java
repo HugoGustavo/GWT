c6bd8f53a505637a50713ce7e77a79e77b8ebafc
HugoGustavo <hugog.valin@gmail.com>
2018-09-10 07:35:27 -0300

    Celltree widget

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -1,89 +1,54 @@
 package com.tutorialspoint.client;
 
 
+import java.util.Date;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Command;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.MenuBar;
-import com.google.gwt.user.client.ui.MenuItem;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.datepicker.client.DateBox;
+import com.google.gwt.user.datepicker.client.DatePicker;
+
 
 public class HelloWorld implements EntryPoint {
 
-	private void showSelectedMenuItem(String menuItemName) {
-		Window.alert("Menu item: " + menuItemName + " selected");
-	}
 	
 	@Override
 	public void onModuleLoad() {
-		MenuBar menu = new MenuBar();
-		menu.setAutoOpen(true);
-		menu.setWidth("100px");
-		menu.setAnimationEnabled(true);
+		DatePicker datePicker = new DatePicker();
+		final Label text = new Label();
 		
-		MenuBar fileMenu = new MenuBar(true);
-		fileMenu.setAnimationEnabled(true);
-		
-		fileMenu.addItem("New", new Command() {
+		datePicker.addValueChangeHandler(new ValueChangeHandler<Date>() {
 			@Override
-			public void execute() {
-				showSelectedMenuItem("New");
+			public void onValueChange(ValueChangeEvent<Date> event) {
+				Date date = event.getValue();
+				String dateString = DateTimeFormat.getFormat("MM/dd/yyyy").format(date);
+				text.setText(dateString);
 			}
 		});
 		
-		fileMenu.addSeparator();
-		fileMenu.addItem("Exit", new Command() {
-			@Override
-			public void execute() {
-				showSelectedMenuItem("Exit")
-			}
-		});
+		datePicker.setValue(new Date(), true);
 		
-		MenuBar editMenu = new MenuBar(true);
-		editMenu.setAnimationEnabled(true);
+		DateTimeFormat dateFormat = DateTimeFormat.getFormat("MM/dd/yyyy");
+		DateBox dateBox = new DateBox();
+		dateBox.setFormat(new DateBox.DefaultFormat(dateFormat));
 		
-		editMenu.addItem("Undo", new Command() {
-			@Override
-			public void execute() {
-				showSelectedMenuItem("Undo");
-			}
-		});
+		Label selectLabel = new Label("Permanent DatePicker: ");
+		Label selectLabel2 = new Label("DateBox with popup DatePicker: ");
 		
-		editMenu.addItem("Redo", new Command() {
-			@Override
-			public void execute() {
-				showSelectedMenuItem("Redo");
-			}
-		});
+		VerticalPanel vPanel = new VerticalPanel();
+		vPanel.setSpacing(10);
+		vPanel.add(selectLabel);
+		vPanel.add(text);
+		vPanel.add(datePicker);
+		vPanel.add(selectLabel2);
+		vPanel.add(dateBox);
 		
-		editMenu.addItem("Cut", new Command() {
-			@Override
-			public void execute() {
-				showSelectedMenuItem("Cut");
-			}
-		});
-		
-		editMenu.addItem("Copy", new Command() {
-			@Override
-			public void execute() {
-				showSelectedMenuItem("Copy");
-			}
-		});
-		
-		editMenu.addItem("Paste", new Command() {
-			@Override
-			public void execute(){
-				showSelectedMenuItem("Paste");
-			}
-		});
-		
-		menu.addItem(new MenuItem("File"), fileMenu);
-		menu.addSeparator();
-		menu.addItem(new MenuItem("Edit"), editMenu);
-		
-		RootPanel.get("gwtContainer").add(menu);
-		
-		
+		RootPanel.get("gwtContainer").add(vPanel);
 	}
 
 }
