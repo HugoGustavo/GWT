6a158651182354148165dc78ae2b6a6804fd0884
HugoGustavo <hugog.valin@gmail.com>
2018-09-10 09:37:32 -0300

    HorizontalSplitPanel

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,25 +2,30 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalSplitPanel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 public class HelloWorld implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		VerticalPanel verticalPanel = new VerticalPanel();
-		for ( int i = 1; i <= 10; i++) {
-			CheckBox checkBox = new CheckBox("Item " + i);
-			verticalPanel.add(checkBox);
+		HorizontalSplitPanel horizontalSplitPanel = new HorizontalSplitPanel();
+		horizontalSplitPanel.setSize("300px", "200px");
+		horizontalSplitPanel.setSplitPosition("30%");
+		
+		String randomText = "This is a sample text. ";
+		for ( int i = 0; i < 2;  i++) {
+			randomText += randomText;
 		}
 		
-		DecoratorPanel decoratorPanel = new DecoratorPanel();
-		decoratorPanel.add(verticalPanel);
+		horizontalSplitPanel.setRightWidget(new HTML(randomText));
+		horizontalSplitPanel.setLeftWidget(new HTML(randomText));
 		
+		DecoratorPanel decoratorPanel = new DecoratorPanel();
+		decoratorPanel.add(horizontalSplitPanel);
 		RootPanel.get("gwtContainer").add(decoratorPanel);
 	}
 
