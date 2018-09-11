08dd3d2ad2a76dcf9ae315de35a926d3808a3387
HugoGustavo <hugog.valin@gmail.com>
2018-09-11 10:00:41 -0300

    History class

--- src/com/tutorialspoint/client/HelloWorld.java
+++ src/com/tutorialspoint/client/HelloWorld.java
@@ -2,72 +2,65 @@ package com.tutorialspoint.client;
 
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DecoratorPanel;
-import com.google.gwt.user.client.ui.HasHorizontalAlignment;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.logical.shared.SelectionEvent;
+import com.google.gwt.event.logical.shared.SelectionHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.TabPanel;
 
 
 public class HelloWorld implements EntryPoint {
-	private HelloWorldMessages messages = GWT.create(HelloWorld.class);
-	
-	String getGreeting(String name) {
-		return "Hello " + name + "!";
-	}
-	
+
 	@Override
 	public void onModuleLoad() {
-	      /*create UI */
-	      final TextBox txtName = new TextBox(); 
-	      txtName.setWidth("200");
-	      txtName.addKeyUpHandler(new KeyUpHandler() {
-	         @Override
-	         public void onKeyUp(KeyUpEvent event) {
-	            if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER){
-	               Window.alert(getGreeting(txtName.getValue()));
-	            }				
-	         }
-	      });
-	      Label lblName = new Label(messages.enterName() + ": ");
-
-	      Button buttonMessage = new Button(messages.clickMe() + "!");
-
-	      buttonMessage.addClickHandler(new ClickHandler() {			
-	         @Override
-	         public void onClick(ClickEvent event) {
-	            Window.alert(getGreeting(txtName.getValue()));
-	         }
-	      });
-
-	      HorizontalPanel hPanel = new HorizontalPanel();	
-	      hPanel.add(lblName);
-	      hPanel.add(txtName);      
-
-	      VerticalPanel vPanel = new VerticalPanel();
-	      vPanel.setSpacing(10);
-	      vPanel.add(hPanel);
-	      vPanel.add(buttonMessage);
-	      vPanel.setCellHorizontalAlignment(buttonMessage, 
-	      HasHorizontalAlignment.ALIGN_RIGHT);
-
-	      DecoratorPanel panel = new DecoratorPanel();
-	      panel.add(vPanel);
-	      Label titleLabel = new Label(messages.applicationTitle());
-	      //Add title to the application
-	      RootPanel.get("gwtAppTitle").add(titleLabel);
-	      // Add widgets to the root panel.
-	      RootPanel.get("gwtContainer").add(panel);
-	   }
+		final TabPanel tabPanel = new TabPanel();
+		
+		HTML firstPage = new HTML("<h1>We are on first Page.</h1>");
+		HTML secondPage = new HTML("<h1>We are on second Page.</h1>");
+		HTML thirdPage = new HTML("<h1>We are on third Page.</h1>");
+		
+		String firstPageTitle = "First Page";
+		String secondPageTitle = "Second Page";
+		String thirdPageTitle = "Third Page";
+		
+		tabPanel.setWidth("400");
+		
+		tabPanel.add(firstPage, firstPageTitle);
+		tabPanel.add(secondPage, secondPageTitle);
+		tabPanel.add(thirdPage, thirdPageTitle);
+		
+		tabPanel.addSelectionHandler(new SelectionHandler<Integer>() {
+			@Override
+			public void onSelection(SelectionEvent<Integer> event) {
+				History.newItem("pageIndex: " + event.getSelectedItem());
+			}
+		});
+		
+		History.addValueChangeHandler(new ValueChangeHandler<String>() {
+			@Override
+			public void onValueChange(ValueChangeEvent<String> event) {
+				String historyToken = event.getValue();
+				try {
+					if ( historyToken.substring(0,9).equals("pageIndex")) {
+						String tabIndexToken = historyToken.substring(9,10);
+						int tabIndex = Integer.parseInt(tabIndexToken);
+						tabPanel.selectTab(tabIndex);
+					} else {
+						tabPanel.selectTab(0);
+					}
+				} catch (IndexOutOfBoundsException e) {
+					tabPanel.selectTab(0);
+				}
+			}
+		});
+		
+		tabPanel.selectTab(0);
+		
+		RootPanel.get("gwtContainer").add(tabPanel);
+		
+	}
 
 }
